c576d5b8134e9cb507734da488b397f7daddcf82
Jean-Paul Khawam <jeanpaulkhawam@protonmail.com>
2023-06-03 13:49:36 +0300

    add astro-i18next, astro-seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://firasalalam.clinic",
-  integrations: [tailwind(), image({
-    serviceEntryPoint: "@astrojs/image/sharp"
-  }), sitemap(), react()]
+  integrations: [
+    tailwind(),
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    sitemap(),
+    react(),
+    astroI18next(),
+  ],
 });
