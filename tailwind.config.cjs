6c458c167bd1fbbc3683c9c46671e7ff06a506d0
Jean-Paul Khawam <jeanpaulkhawam@protonmail.com>
2023-06-15 15:00:08 +0300

    set dark mode to class

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
+  darkMode: 'class',
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
