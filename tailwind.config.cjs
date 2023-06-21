256d1d365afba5e1c70841499bd0b5b01a9ccc7b
JP <jeanpaulkhawam@protonmail.com>
2023-06-21 13:41:49 +0300

    enable JIT compiler

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,9 @@
 const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
-  darkMode: 'class',
+  mode: "jit",
+  purge: ["./src/**/*.{js,jsx,ts,tsx,vue,astro}"],
+  darkMode: "class",
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
