414c03da6160b564994484e7817743a3729f179c
JP <jeanpaulkhawam@protonmail.com>
2023-07-05 16:06:07 +0300

    remove unneeded pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,9 +27,7 @@ export default defineConfig({
         filter: (page) =>
           page !== "https://firasalalam.clinic/thanks" &&
           page !== "https://firasalalam.clinic/404" &&
-          page !== "https://firasalalam.clinic/certificates" &&
-          page !== "https://firasalalam.clinic/skills-building" &&
-          page !== "https://firasalalam.clinic/targeted-difficulties",
+          page !== "https://firasalalam.clinic/certificates" 
       }
     ),
     react(),
